378f8bdf4d66c3c103b13740a1d23551139bc7a8
Shreyansh shekhar Dwivedi <85301920+SHREYANSH11012004@users.noreply.github.com>
2026-09-06 20:05:00 +0000

    fix jwt error

--- backend/src/main/java/com/garba/partner/config/SecurityConfig.java
+++ backend/src/main/java/com/garba/partner/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.garba.partner.config;
 
+import com.garba.partner.security.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
